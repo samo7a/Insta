b724cda006f45d179c2c268ccccb8cbf1a54ae13
samo7a <aelshetany@knights.ucf.edu>
2021-10-04 17:03:13 -0400

    Initial Commit

---
+++ Insta/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Insta
+//
+//  Created by Ahmed  Elshetany  on 10/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
